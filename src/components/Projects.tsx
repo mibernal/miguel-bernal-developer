96f63325b4fcceae7fdeb5134a79b4c07835fa81
miguel-bernal-developer <miguelangelbernal1991@gmail.com>
2025-10-25 11:09:19 -0500

    Cambio Componente

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -286,7 +286,7 @@ const Projects = () => {
         
         <div className="text-center mt-12">
           <Button variant="outline" size="lg" className="border-primary/30 hover:border-primary" asChild>
-            <a href="https://github.com/miguelangelbernal" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/mibernal" target="_blank" rel="noopener noreferrer">
               <Github className="w-5 h-5 mr-2" />
               Ver todos en GitHub
             </a>
